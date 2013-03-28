002519dddbfe1ac749ae440f9b39fcdf695f5f4e
Phoenyx89 <alessio.pas89@gmail.com>
2013-03-28 12:22:51 +0100

    cerchio

---
+++ plasm-js/2013-03-28.js
@@ -0,0 +1,33 @@
+// un rettangolo diviso in 32 
+var domain2 = DOMAIN([[0,PI], [0,1]])([32, 2]);
+DRAW(SKELETON(1)(domain2);
+
+var domain = DOMAIN([[0,10]])([10]); // dominio da 0 a 10 diviso in 10 intervalli
+var mapping = function(v) {
+	return [v[0], v[0]];
+};
+
+var model = MAP(mapping)(domain); // bisettrice primo terzo quadrante
+
+// ora in 3D
+var mappings = [];
+
+//per le x
+mappings[0] = function(v) {
+	return [v[0]];
+};
+
+//per le y
+mappings[1] = function(v) {
+	return [v[0]];
+};
+
+// per le z
+mappings[2] = function(v) {
+	return [v[0]]; 
+};
+
+var model1 = MAP(mappings)(domain);
+
+
+
